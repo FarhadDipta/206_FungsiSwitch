690c4468c2e098597f0248d068f5ba488a5ae81a
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-03-26 14:29:09 +0700

    int penjumlahan

--- calculator.cpp
+++ calculator.cpp
@@ -3,12 +3,17 @@ using namespace std;
 
 int bilangan1, bilangan2;
 
-void input()
+void inputData()
 {
     cout << "Masukan bilangan pertama :";
     cin >> bilangan1;
     cout << "Masukan bilangan kedua : ";
     cin >> bilangan2;
+}
+
+int penjumlahan(int a, int b)
+{
+    return a + b;
 }
 
 int main(){
