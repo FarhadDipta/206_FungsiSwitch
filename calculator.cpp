bc570524cb93a6f4546722dad62d6e308c61ddb0
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-03-26 14:51:05 +0700

    output pertama calculator

--- calculator.cpp
+++ calculator.cpp
@@ -26,12 +26,26 @@ int perkalian(int a, int b)
     return a * b;
 }
 
-int pembagian(int a, int b)
+float pembagian(int a, int b)
 {
     return a / b;
 }
 
-
-int main(){
-
+int main()
+{
+    int pilihan;
+    
+    do
+    {
+        cout << "Kalkulator Sederhana" << endl;
+        cout << "====================" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "====================" << endl;
+        cout << "Masukan menu pilihan : ";
+        cin >> pilihan;
+    } while (pilihan != 5);
 }
