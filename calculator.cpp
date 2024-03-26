e34b685681ed3782cd18a529fdb6c1331c13b705
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-03-26 14:26:22 +0700

    void input

---
+++ calculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int bilangan1, bilangan2;
+
+void input()
+{
+    cout << "Masukan bilangan pertama :";
+    cin >> bilangan1;
+    cout << "Masukan bilangan kedua : ";
+    cin >> bilangan2;
+}
+
+int main(){
+
+}
