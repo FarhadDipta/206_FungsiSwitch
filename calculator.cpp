7bdcf47cb50604113a8ce7ad6a307d5dc69fbd70
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-03-26 14:55:50 +0700

    switch 2

--- calculator.cpp
+++ calculator.cpp
@@ -51,7 +51,26 @@ int main()
         switch (pilihan)
         {
         case 1 : 
-        
+        inputData();
+        cout << "Hasil penjumlahan = " << penjumlahan(bilangan1, bilangan2) << endl;
+            break;
+
+        case 2 : 
+        inputData();
+        cout << "Hasil pengurangan = " << pengurangan(bilangan1, bilangan2) << endl;
+            break;
+
+        case 3 : 
+        inputData();
+        cout << "Hasil perkalian = " << perkalian(bilangan1, bilangan2) << endl;
+            break;
+
+        case 4 : 
+        inputData();
+        cout << "Hasil pembagian = " << pembagian(bilangan1, bilangan2) << endl;
+            break;
+
+        case 5 : 
             break;
         
         default:
